9ce035a2cce86089be6e3adf6ee8f5d973cb2b18
Vlad Ciphersky <97faizov@gmail.com>
2019-01-26 20:54:19 +0500

    Update ciphersky.js

--- ciphersky.js
+++ ciphersky.js
@@ -3,7 +3,7 @@ var request = require('request');
 class ciphersky {
 	shorturl(url) {
 		return new Promise((resolve, reject) => {
-			request(`https://api.lvsh.tk/short.php?url=${url}`, (e, r, b) => {
+			request(`https://api.lvsh.tk/short.php?url=${url}`, { json: true }, (e, r, b) => {
 				resolve(b);
 			});
 		});
