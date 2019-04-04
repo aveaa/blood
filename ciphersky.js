989d240d24511af75df8dedb938e4628d47a84aa
Vlad Ciphersky <97faizov@gmail.com>
2019-04-04 10:31:36 +0500

    кпц, чё за корявый код

--- ciphersky.js
+++ ciphersky.js
@@ -2,5 +2,6 @@ module.exports = (class ciphersky {
 	async shorturl(url) {
 		require('request')(`https://api.lvsh.tk/short.php?url=${url}`, { json: true }, (e, r, b) => {
 			return b;
-		}
-	})
+		});
+	}
+})
